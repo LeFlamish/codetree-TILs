58cd1c8711acf4a83d4783003ecf540a3eb21501
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-03 01:17:01 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240703/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+void printStar(int N) {
+    if (N == 0) return;
+
+    printStar(N - 1);
+    for (int i = 0; i < N; i++) cout << '*';
+    cout << '\n';
+}
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> N;
+    printStar(N);
+    return 0;
+}

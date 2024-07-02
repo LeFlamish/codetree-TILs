b1aff20711d4522bc059c53c5eaf3c38268d2da4
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-02 19:51:46 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240702/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void printing(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "12345^&*()_\n";
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    printing(n);
+    return 0;
+}

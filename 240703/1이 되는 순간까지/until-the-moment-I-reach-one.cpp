13f19e2b616839b40175b864762b8f7e1458a15a
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-03 03:20:49 +0900

    Title: 1이 되는 순간까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240703/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
+++ 240703/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -6,13 +6,13 @@ int N;
 int calculate(int N, int cnt) {
     if (N == 1) return cnt;
 
-    if (N % 2) return calculate(N / 2, cnt + 1);
-    else return calculate(N / 3, cnt + 1);
+    if (N % 2) return calculate(N / 3, cnt + 1);
+    else return calculate(N / 2, cnt + 1);
 }
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> N;
-    cout << calculate(N, 1);
+    cout << calculate(N, 0);
     return 0;
 }

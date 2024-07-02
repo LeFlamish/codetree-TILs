2bea605e4bebaff4fd128518d2aa585eb699ec7f
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-03 01:07:53 +0900

    Update repeated-output-2.cpp

--- 240703/반복 출력하기 2/repeated-output-2.cpp
+++ 240703/반복 출력하기 2/repeated-output-2.cpp
@@ -15,3 +15,25 @@ int main() {
     printHello(N);
     return 0;
 }
+
+// 코드트리 풀이 : 재귀함수 호출 위치가 다름.
+#include <iostream>
+
+using namespace std;
+
+void PrintString(int n) { // 1부터 n번째 줄까지 문자열을 출력하는 함수
+    if(n == 0)            // n이 0이라면, 더 이상 진행하지 않고
+        return;           // 퇴각합니다.
+
+    PrintString(n - 1);   // 1부터 n - 1번째 줄까지 출력하는 함수
+    cout << "HelloWorld" << endl;
+}
+
+int main() {
+    // 변수 선언 및 입력:
+    int n;
+    cin >> n;
+
+    PrintString(n);
+    return 0;
+}

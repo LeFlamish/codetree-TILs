d7ee2db842aae505958be332a2f83f28fc93e021
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-02 23:32:39 +0900

    Update to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp

--- 240702/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 240702/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -44,3 +44,39 @@ int main() {
 
     return 0;
 }
+
+// 내 풀이
+#include <iostream>
+using namespace std;
+int A[101], B[101], n1, n2;
+
+void subseq() {
+    for (int i = 0; i < n1 - n2 + 1; i++) {
+        bool flag = true;
+        for (int j = 0; j < n2; j++) {
+            if (A[i + j] != B[j]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            cout << "Yes";
+            return;
+        }
+    }
+    cout << "No";
+    return;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> n1 >> n2;
+    for (int i = 0; i < n1; i++) {
+        cin >> A[i];
+    }
+    for (int i = 0; i < n2; i++) {
+        cin >> B[i];
+    }
+    subseq
+    return 0;
+}

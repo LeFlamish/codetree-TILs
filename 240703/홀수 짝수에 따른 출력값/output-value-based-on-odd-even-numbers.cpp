2f017a2a7ef9cdeee83551094ef18f930a57ef99
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-03 03:36:50 +0900

    Update output-value-based-on-odd-even-numbers.cpp

--- 240703/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
+++ 240703/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
@@ -15,3 +15,28 @@ int main() {
     cout << Sum(N, 0);
     return 0;
 }
+
+// 코드트리 풀이
+#include <iostream>
+
+using namespace std;
+
+// 1부터 n까지의 n과 홀짝이 같은 수들의 합을 반환합니다.
+int GetNum(int n) {
+    if(n == 1)
+        return 1;
+    if(n == 2)
+        return 2;
+
+    // n과 홀짝이 같은 수만을 재귀함수로 호출합니다.
+    return GetNum(n - 2) + n;
+}
+
+int main() {
+    // 변수 선언 및 입력:
+    int n;
+    cin >> n;
+
+    cout << GetNum(n);
+    return 0;
+}

d19a1ce939121c2ec238b9733f37ee585b546b22
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-03 03:50:44 +0900

    Update maximum-value-with-recursive-function.cpp

--- 240703/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
+++ 240703/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -19,3 +19,34 @@ int main() {
     cout << findMax(N, 0);
     return 0;
 }
+
+// 코드트리 풀이
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 100
+
+using namespace std;
+
+int arr[MAX_N];
+
+// a번째 까지 인덱스의 숫자 중에 가장 큰 값을 반환합니다.
+int MaxValue(int a) {
+    if(a == 0)
+        return arr[0];
+
+    return max(MaxValue(a - 1), arr[a]);
+}
+
+int main() {
+    // 변수 선언 및 입력:
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << MaxValue(n - 1);
+
+    return 0;
+}

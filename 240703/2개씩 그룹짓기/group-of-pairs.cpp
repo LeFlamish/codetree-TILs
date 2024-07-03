6eaeb989cfc0dfceec092a1e24555137dbd0409e
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-03 13:49:49 +0900

    Title: 2개씩 그룹짓기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240703/2개씩 그룹짓기/group-of-pairs.cpp
+++ 240703/2개씩 그룹짓기/group-of-pairs.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-//inputArray에서 최댓값과 최솟값을 묶어서 원소 2개짜리 배열을 만들때 조건 만족
-//삽입정렬을 통해서 inputArray의 원소들을 정렬할 계획
+#define MAX_N 1000
+
 int main(){
-    int N;
-    cin>>N;
-    int inputArray[2002];
-    for(int i=0;i<2*N;i++){
-        cin>>inputArray[i];
+    int n;
+    cin >> n;
+
+    int nums[2 * MAX_N];
+
+    for(int i = 0; i < 2 * n; i++)
+        cin >> nums[i];
+
+    // nums[0]부터 nums[2n - 1]까지를 정렬함
+    sort(nums, nums + 2 * n);
+
+    int group_max = 0;
+    for(int i = 0; i < n; i++) {
+        // i번째와 2n - 1 - i번째 원소를 매칭
+        int group_sum = nums[i] + nums[2*n - 1 - i];
+        if(group_sum > group_max)
+            // 최댓값 갱신
+            group_max = group_sum;
     }
-    //inputArray 배열의 맨 처음 원소를 key라고 지정
-    //key다음 원소값과 key값을 비교한다
-    for(int x=1;x<2*N;x++){
-        int key=inputArray[x];
-        //key 다다음 원소=inputArray[2]값과 지금까지 정렬한 부분을 비교한다
-        for(int y=x-1;y>=0;y--){
-            if(inputArray[y]>key){ //key의 앞자리를 하나씩 비교
-                inputArray[y+1]=inputArray[y]; //비교결과 앞자리가 작으면 한칸씩 밀어서 빈칸을 만들어준다
-            }else{
-                break;
-            }
-            inputArray[y]=key;//key값 업데이트
-        }
-        
-    }
-    int group_max=0;
-    //intArray[0]+intArray[-1]하면 정의에 어긋남
-    for(int i=0;i<2*N;i++){
-        //i번째와 (2n-1-i)번째 원소 매칭 -> (5+5)경우의 수는 만들수 없음
-        int group_sum=inputArray[i] + inputArray[2*N-1-i];
-        if(group_sum>group_max)
-            group_max=group_sum;
-    }
-    cout<<group_max;
+
+    cout << group_max;
+
+    return 0;
 }

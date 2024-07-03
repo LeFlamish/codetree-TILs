80866e1b908331fd133202f5be0c1a987eb2ed84
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-03 14:08:10 +0900

    Title: 정사각형의 넓이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240703/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int n;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> n;
+    if (n < 5) cout << "tiny";
+    else cout << n * n;
+    return 0;
+}

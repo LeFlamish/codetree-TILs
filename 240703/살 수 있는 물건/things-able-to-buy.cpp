431813be7feed755e709365d53e30885fd2a486d
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-03 14:15:10 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240703/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int n;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> n;
+    if (n >= 3000) cout << "book";
+    else if (n >= 1000) cout << "mask";
+    else cout << "no";
+    return 0;
+}

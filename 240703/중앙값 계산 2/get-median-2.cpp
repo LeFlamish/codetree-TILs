14b9890366e68da5f0f3c17ecc116c95b3cba737
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-03 17:22:55 +0900

    Update get-median-2.cpp

--- 240703/중앙값 계산 2/get-median-2.cpp
+++ 240703/중앙값 계산 2/get-median-2.cpp
@@ -19,3 +19,33 @@ int main() {
     }
     return 0;
 }
+
+// 코드트리 풀이
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 100
+
+using namespace std;
+
+// 변수 선언
+int n;
+int arr[MAX_N];
+
+int main() {
+    // 입력:
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    // 홀수 번째 수를 지날때마다 정렬을 진행한 후 가운데 값을 출력합니다.
+    for(int i = 0; i < n; i++) {
+        if(i % 2 == 0) {
+            // 오름차순 정렬을 진행합니다.
+            sort(arr, arr + i + 1);
+            // 가운데 값을 출력합니다.
+            cout << arr[i / 2] << " ";
+        }
+    }
+    return 0;
+}

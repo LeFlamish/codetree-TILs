9669e365ab5f96c5e1f8b8d1758550af816945e4
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-04 13:20:03 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240704/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int m1, m2, d1, d2, x, y, gap, cnt;
+int num_of_days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int psum[13];
+string target;
+string weekday[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> m1 >> d1 >> m2 >> d2 >> target;
+    for (int i = 1; i <= 12; i++) {
+        psum[i] = psum[i-1] + num_of_days[i];
+    }
+    x = psum[m1-1] + d1;
+    y = psum[m2-1] + d2;
+    gap = y - x;
+    cnt++;
+    if (target == "Tue") gap -= 1;
+    else if (target == "Wed") gap -= 2;
+    else if (target == "Thu") gap -= 3;
+    else if (target == "Fri") gap -= 4;
+    else if (target == "Sat") gap -= 5;
+    else if (target == "Sun") gap -= 6;
+    cnt += gap / 7;
+    cout << cnt;
+    return 0;
+}

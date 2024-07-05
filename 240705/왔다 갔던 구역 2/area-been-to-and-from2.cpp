c538f227689289913f9184132e8ed4fb1489bd44
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-05 20:33:39 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240705/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240705/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,25 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int n, arr[2002], cur = 1001, cnt;
+int n, arr[2002], cur, nxt, cnt;
 int main() {
-    cin.tie(0)->sync_with_stdio(0);
+	cin.tie(0)->sync_with_stdio(0);
     cin >> n;
+    cur = 1001;
     for (int i = 0; i < n; i++) {
         int x;
         char y;
         cin >> x >> y;
         if (y == 'R') {
-            for (int j = cur; j < cur + x; j++) arr[j]++;
-            cur += x;
+            nxt = cur + x;
+            for (int j = cur; j < nxt; j++) arr[j]++;
+            cur = nxt;
         }
         else {
-            for (int j = cur; j > cur - x; j--) arr[j]++;
-            cur -= x;
+            nxt = cur - x;
+            for (int j = cur; j > nxt; j--) arr[j]++;
+            cur = nxt;
         }
     }
     for (int i = 1; i < 2002; i++) {
         if (arr[i] >= 2) cnt++;
     }
     cout << cnt;
-    return 0;
+	return 0;
 }

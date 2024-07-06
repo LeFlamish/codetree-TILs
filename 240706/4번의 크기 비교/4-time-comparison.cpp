3fab69fcf5051fe5c4c9541738b54f5ea0a22a8e
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-06 19:23:20 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240706/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int a, b, c, d, e;
+int main() {
+    cin >> a >> b >> c >> d >> e;
+    cout << (a > b) << '\n';
+    cout << (a > c) << '\n';
+    cout << (a > d) << '\n';
+    cout << (a > e) << '\n';
+    return 0;
+}

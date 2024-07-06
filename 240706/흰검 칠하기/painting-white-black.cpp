d1b51ac1311e82e9e9a44f06bc3e8ece0cd970ab
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-06 23:59:15 +0900

    Title: 흰검 칠하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240706/흰검 칠하기/painting-white-black.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, arr[200002][2], cur, nxt, cnt[3];
+char color[200002];
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin >> n;
+	cur = 100001;
+	for (int t = 0; t < n; t++) {
+		int x;
+		char y;
+		cin >> x >> y;
+		if (y == 'R') {
+			nxt = cur + x - 1;
+			for (int i = cur; i <= nxt; i++) {
+				arr[i][0]++;
+				color[i] = 'B';
+			}
+		}
+		else {
+			nxt = cur - x + 1;
+			for (int i = cur; i >= nxt; i--) {
+				arr[i][1]++;
+				color[i] = 'W';
+			}
+		}
+		cur = nxt;
+	}
+	for (int i = 1; i < 200002; i++) {
+		if (arr[i][0] >= 2 && arr[i][1] >= 2) cnt[2]++;
+		else if (arr[i][0] > 0 || arr[i][1] > 0) {
+			if (color[i] == 'W') cnt[0]++;
+			else cnt[1]++;
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << cnt[i] << ' ';
+	}
+	return 0;
+}

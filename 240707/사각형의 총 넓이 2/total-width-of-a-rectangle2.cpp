3772a4e0b19dd82a8a7d5eb8207d369b20b3d4f9
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-07 20:15:52 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240707/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 240707/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
-int N, board[101][101], X1, Y1, X2, Y2, cnt;
+int N, board[202][202], X1, Y1, X2, Y2, cnt;
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> N;
     for (int n = 0; n < N; n++) {
         cin >> X1 >> Y1 >> X2 >> Y2;
-        for (int i = X1; i < X2; i++) {
-            for (int j = Y1; j < Y2; j++) {
+        for (int i = X1 + 101; i < X2 + 101; i++) {
+            for (int j = Y1 + 101; j < Y2 + 101; j++) {
                 board[i][j] = 1;
             }
         }
     }
-    for (int i = 0; i < 101; i++) {
-        for (int j = 0; j < 101; j++) {
+    for (int i = 0; i < 202; i++) {
+        for (int j = 0; j < 202; j++) {
             if (board[i][j]) cnt++;
         }
     }

4f7378c22b7ec6860576621f977c219ec666c853
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-08 23:26:49 +0900

    Title: 선두를 지켜라, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240708/선두를 지켜라/keep-the-lead.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, M, A[1000001], B[1000001], v, t, idx = 1, total, cnt;
+char Prev, Cur;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> v >> t;
+        total += t;
+        for (int j = 0; j < t; j++) {
+            A[idx + j] = A[idx + j - 1] + v;
+        }
+        idx += t;
+    }
+    idx = 1;
+    for (int i = 0; i < M; i++) {
+        cin >> v >> t;
+        for (int j = 0; j < t; j++) {
+            B[idx + j] = B[idx + j - 1] + v;
+        }
+        idx += t;
+    }
+    for (int i = 0; i <= total; i++) {
+        if (A[i] < B[i]) Cur = 'B';
+        else if (A[i] > B[i]) Cur = 'A';
+        if (Prev != Cur) cnt++;
+        Prev = Cur;
+    }
+    cout << cnt - 1;
+    return 0;
+}

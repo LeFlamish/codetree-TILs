4de5698df269c8280729e68298cffd36c43f00d7
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-08 18:55:06 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240708/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, cur, Prev, cnt = 1, ret = 1;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> N;
+    cin >> Prev;
+    for (int i = 1; i < N; i++) {
+        cin >> cur;
+        if (Prev == cur) cnt++;
+        else {
+            ret = max(ret, cnt);
+            cnt = 1;
+        }
+        Prev = cur;
+    }
+    cout << ret;
+    return 0;
+}

e19e37d17dd170e714429cad3ad14021052b38b6
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-09 23:26:04 +0900

    Title: 이상한 진수 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240709/이상한 진수 2/awkward-digits-2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int ret;
+bool flag;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '0') {
+            s[i] = '1';
+            flag = true;
+            break;
+        }
+    }
+    if (!flag) {
+        s[s.size() - 1] = 0;
+    }
+    for (int i = 0; i < s.size(); i++) {
+        ret = (s[i] - '0') + 2 * ret;
+    }
+    cout << ret;
+    return 0;
+}

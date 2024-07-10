267f75868d5fa8e274276313c214ac04c06afd78
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-10 23:21:02 +0900

    Title: 최고의 13위치 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240710/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, board[20][20], cnt, ret;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> board[i][j];
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            for (int a = 0; a < N - 2; a++) {
+                for (int b = 0; b < N - 2; b++) {
+                    ret = max(board[i][a] + board[i][a + 1] + board[i][a + 2] + board[j][b] + board[j][b + 1] + board[j][b + 2], ret);
+                }
+            }
+        }
+    }
+    cout << ret;
+    return 0;
+}

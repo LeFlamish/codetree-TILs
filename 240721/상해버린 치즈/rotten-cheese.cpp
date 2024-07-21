ea63b2516e52dba1221de00c8fbdd69d52f54a90
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-21 20:24:30 +0900

    Title: 상해버린 치즈, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240721/상해버린 치즈/rotten-cheese.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> P;
+#define when first
+#define what second
+int N, M, D, S, ret;
+vector<P> record[51];
+vector<int> problem;
+int sick[51], cheese[51];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> N >> M >> D >> S;
+    for (int i = 0; i < D; i++) {
+        int p, m, t;
+        cin >> p >> m >> t;
+        record[p].push_back({ t, m });
+    }
+    for (int i = 0; i < S; i++) {
+        int p, t;
+        cin >> p >> t;
+        sick[p] = t;
+    }
+    for (int i = 1; i <= N; i++) {
+        if (sick[i]) {
+            for (int j = 0; j < record[i].size(); j++) {
+                cheese[record[i][j].what]++;
+            }
+        }
+    }
+    for (int i = 1; i <= M; i++) {
+        if (cheese[i] == S) problem.push_back(i);
+    }
+    for (int i = 0; i < problem.size(); i++) {
+        int cnt = 0;
+        for (int p = 1; p <= N; p++) {
+            for (int t = 0; t < record[p].size(); t++) {
+                if (record[p][t].what == problem[i]) cnt++;
+            }
+        }
+        ret = max(cnt, ret);
+    }
+    cout << ret;
+    return 0;
+}

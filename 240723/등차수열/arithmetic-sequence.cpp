f2e0c202c2f94270b177ea3bfadade5dfe6fa3bb
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-23 01:58:38 +0900

    Title: 등차수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/등차수열/arithmetic-sequence.cpp
+++ 240723/등차수열/arithmetic-sequence.cpp
@@ -1,20 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, arr[100], ret;
+int N, arr[100], ret, mx = INT_MIN, mn = INT_MAX, cnt;
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> arr[i];
+        mx = max(arr[i], mx);
+        mn = min(arr[i], mn);
     }
-    for (int i = 0; i < N; i++) {
-        for (int j = i + 1; j < N; j++) {
-            int from = min(arr[i], arr[j]);
-            int to = max(arr[i], arr[j]);
-            for (int k = from; k <= to; k++) {
-                if (to - k == k - from) ret++;
+    for (int k = mn; k <= mx; k++) {
+        cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                int from = min(arr[i], arr[j]);
+                int to = max(arr[i], arr[j]);
+                if (abs(arr[i] - k) == abs(arr[j] - k)) cnt++;
             }
         }
+        ret = max(ret, cnt);
     }
     cout << ret;
     return 0;

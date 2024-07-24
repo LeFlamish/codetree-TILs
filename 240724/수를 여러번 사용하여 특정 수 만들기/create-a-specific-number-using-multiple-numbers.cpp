4d6acdcce18b12e8ee181c2cb32688d196104b46
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-24 13:07:58 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, c, mxa, mxb, ret;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> a >> b >> c;
+    mxa = (c + a - 1) / a;
+    mxb = (c + b - 1) / b;
+    for (int i = 0; i < mxa; i++) {
+        for (int j = 0; j < mxb; j++) {
+            int tmp = i * a + j * b;
+            if (tmp < c) ret = max(ret, tmp);
+        }
+    }
+    cout << ret;
+    return 0;
+}

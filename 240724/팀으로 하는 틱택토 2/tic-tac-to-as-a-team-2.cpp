241fc2442ac5f99efc7620a2d5eec4b87faff568
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-24 22:20:40 +0900

    Title: 팀으로 하는 틱택토 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, board[3][3], visited[10], ret;
+
+bool checkHorizontal(int start) {
+    memset(visited, 0, sizeof(visited));
+    int a = 0, b = 0;
+    for (int i = 0; i < 3; i++) {
+        visited[board[start][i]]++;
+    }
+    for (int i = 1; i <= 9; i++) {
+        if (visited[i] == 2) a++;
+        if (visited[i] == 1) b++;
+    }
+    if (a == 1 && b == 1) return true;
+    else return false;
+}
+
+bool checkVertical(int start) {
+    memset(visited, 0, sizeof(visited));
+    int a = 0, b = 0;
+    for (int i = 0; i < 3; i++) {
+        visited[board[i][start]]++;
+    }
+    for (int i = 1; i <= 9; i++) {
+        if (visited[i] == 2) a++;
+        if (visited[i] == 1) b++;
+    }
+    if (a == 1 && b == 1) return true;
+    else return false;
+}
+
+bool checkDiagonal1() {
+    memset(visited, 0, sizeof(visited));
+    int a = 0, b = 0;
+    for (int i = 0; i < 3; i++) {
+        visited[board[i][i]]++;
+    }
+    for (int i = 1; i <= 9; i++) {
+        if (visited[i] == 2) a++;
+        if (visited[i] == 1) b++;
+    }
+    if (a == 1 && b == 1) return true;
+    else return false;
+}
+
+bool checkDiagonal2() {
+    memset(visited, 0, sizeof(visited));
+    int a = 0, b = 0;
+    for (int i = 0; i < 3; i++) {
+        visited[board[i][2 - i]]++;
+    }
+    for (int i = 1; i <= 9; i++) {
+        if (visited[i] == 2) a++;
+        if (visited[i] == 1) b++;
+    }
+    if (a == 1 && b == 1) return true;
+    else return false;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    for (int i = 0; i < 3; i++) {
+        cin >> N;
+        board[i][0] = N / 100;
+        board[i][1] = (N % 100) / 10;
+        board[i][2] = N % 10;
+    }
+    for (int i = 0; i < 3; i++) {
+        if (checkHorizontal(i)) ret++;
+        if (checkVertical(i)) ret++;
+    }
+    if (checkDiagonal1()) ret++;
+    if (checkDiagonal2()) ret++;
+    cout << ret;
+    return 0;
+}

aa4b8aa02681fd0285fdb84d71c1d42531710e90
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-09-20 21:48:25 +0900

    Title: 포탑 부수기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240920/포탑 부수기/destroy-the-turret.cpp
+++ 240920/포탑 부수기/destroy-the-turret.cpp
@@ -15,8 +15,6 @@ int dx[] = { 1, 0, -1, 0 };
 int dy[] = { 0, 1, 0, -1 };
 int bombdx[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
 int bombdy[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
-int roaddx[] = { -1, 0, 0, 1 };
-int roaddy[] = { 0, -1, 1, 0 };
 
 void init() {
 	cin >> N >> M >> K;
@@ -28,109 +26,82 @@ void init() {
 }
 
 CD findMin() {
-	vector<CD> V;
-	int Min = INT_MAX;
-	for (int x = M; x > 0; x--) {
-		for (int y = N; y > 0; y--) {
+	CD ret = { 1, 1 };
+	bool flag = false;
+	for (int y = 1; y <= M; y++) {
+		for (int x = 1; x <= N; x++) {
+			if (board[y][x] != 0) {
+				ret = { x, y };
+				flag = true;
+				break;
+			}
+		}
+		if (flag) break;
+	}
+
+	for (int y = 1; y <= M; y++) {
+		for (int x = 1; x <= N; x++) {
 			if (board[y][x] == 0) continue;
-			Min = min(Min, board[y][x]);
-		}
-	}
-	for (int x = M; x > 0; x--) {
-		for (int y = N; y > 0; y--) {
-			if (board[y][x] == Min) V.push_back({ x, y });
-		}
-	}
-	if (V.size() == 1) return V[0];
-	else {
-		vector<CD> tmp;
-		int Max = attack[V[0].Y][V[0].X];
-		for (int i = 0; i < V.size(); i++) {
-			Max = max(Max, attack[V[i].Y][V[i].X]);
-		}
-		for (int i = 0; i < V.size(); i++) {
-			if (attack[V[i].Y][V[i].X] == Max) tmp.push_back({ V[i].X, V[i].Y });
-		}
-		if (tmp.size() == 1) return tmp[0];
-		else {
-			vector<int> A;
-			for (int i = 0; i < tmp.size(); i++) {
-				A.push_back(tmp[i].X + tmp[i].Y);
+			if (board[ret.Y][ret.X] > board[y][x]) {
+				ret = { x, y };
 			}
-			Max = INT_MIN;
-			for (int i : A) {
-				Max = max(Max, i);
-			}
-			vector<CD> B;
-			for (int i = 0; i < A.size(); i++) {
-				if (Max == A[i]) B.push_back(tmp[i]);
-			}
-			if (B.size() == 1) return B[0];
-			else {
-				Max = INT_MIN;
-				vector<CD> C;
-				for (int i = 0; i < B.size(); i++) {
-					Max = max(Max, B[i].X);
+			else if (board[ret.Y][ret.X] == board[y][x]){
+				if (attack[ret.Y][ret.X] < attack[y][x]) {
+					ret = { x, y };
 				}
-				for (int i = 0; i < B.size(); i++) {
-					if (Max == B[i].X) return B[i];
+				else if (attack[ret.Y][ret.X] == attack[y][x]) {
+					if (ret.X + ret.Y < x + y) {
+						ret = { x, y };
+					}
+					else if (ret.X + ret.Y == x + y) {
+						if (ret.X < x) {
+							ret = { x, y };
+						}
+					}
 				}
 			}
 		}
 	}
+	return ret;
 }
 
 CD findMax() {
-	vector<CD> V;
-	int Max = INT_MIN;
-	for (int x = 1; x <= M; x++) {
-		for (int y = 1; y <= N; y++) {
+	CD ret = { 1, 1 };
+	bool flag = false;
+	for (int y = 1; y <= M; y++) {
+		for (int x = 1; x <= N; x++) {
+			if (board[y][x] != 0) {
+				ret = { x, y };
+				flag = true;
+				break;
+			}
+		}
+		if (flag) break;
+	}
+	for (int y = 1; y <= M; y++) {
+		for (int x = 1; x <= N; x++) {
 			if (board[y][x] == 0) continue;
-			Max = max(Max, board[y][x]);
-		}
-	}
-	for (int x = 1; x <= M; x++) {
-		for (int y = 1; y <= N; y++) {
-			if (board[y][x] == Max) V.push_back({ x, y });
-		}
-	}
-	if (V.size() == 1) return V[0];
-	else {
-		vector<CD> tmp;
-		int Min = attack[V[0].Y][V[0].X];
-		for (int i = 0; i < V.size(); i++) {
-			Min = min(Min, attack[V[i].Y][V[i].X]);
-		}
-		for (int i = 0; i < V.size(); i++) {
-			if (attack[V[i].Y][V[i].X] == Min) tmp.push_back({ V[i].X, V[i].Y });
-		}
-		if (tmp.size() == 1) return tmp[0];
-		else {
-			vector<int> A;
-			for (int i = 0; i < tmp.size(); i++) {
-				A.push_back(tmp[i].X + tmp[i].Y);
+			if (board[ret.Y][ret.X] < board[y][x]) {
+				ret = { x, y };
 			}
-			Min = INT_MAX;
-			for (int i : A) {
-				Min = min(Min, i);
-			}
-			vector<CD> B;
-			for (int i = 0; i < A.size(); i++) {
-				if (Min == A[i]) B.push_back(tmp[i]);
-			}
-			if (B.size() == 1) return B[0];
-			else {
-				Min = INT_MAX;
-				vector<CD> C;
-				for (int i = 0; i < B.size(); i++) {
-					Min = min(Min, B[i].X);
+			else if (board[ret.Y][ret.X] == board[y][x]) {
+				if (attack[ret.Y][ret.X] > attack[y][x]) {
+					ret = { x, y };
 				}
-				for (int i = 0; i < B.size(); i++) {
-					if (Min == B[i].X) return B[i];
+				else if (attack[ret.Y][ret.X] == attack[y][x]) {
+					if (ret.X + ret.Y > x + y) {
+						ret = { x, y };
+					}
+					else if (ret.X + ret.Y == x + y) {
+						if (ret.X > x) {
+							ret = { x, y };
+						}
+					}
 				}
 			}
 		}
 	}
+	return ret;
 }
 
 bool canLaser(CD Start, CD End) {
@@ -148,7 +119,7 @@ bool canLaser(CD Start, CD End) {
 			if (nx > M) nx = 1;
 			if (ny < 1) ny = N;
 			if (ny > N) ny = 1;
-			if (board[ny][nx] == 0) continue;
+			if (board[ny][nx] == 0 || visited[ny][nx]) continue;
 			visited[ny][nx] = true;
 			past[ny][nx] = { cur.X, cur.Y };
 			Q.push({ nx, ny });
@@ -162,9 +133,9 @@ vector<CD> findRoad(CD Start, CD End) {
 	int x = End.X;
 	int y = End.Y;
 	while (1) {	
-		if (x == Start.X && y == Start.Y) break;
-		road.push_back(past[y][x]);
 		CD tmp = past[y][x];
+		if (tmp.X == Start.X && tmp.Y == Start.Y) break;
+		road.push_back(past[y][x]);
 		x = tmp.X;
 		y = tmp.Y;
 	}
@@ -175,6 +146,8 @@ vector<CD> findRoad(CD Start, CD End) {
 void bombAttack(CD Start, CD End, int K) {
 	board[End.Y][End.X] -= board[Start.Y][Start.X] + N + M;
 	if (board[End.Y][End.X] < 0) board[End.Y][End.X] = 0;
+	related[Start.Y][Start.X] = true;
+	related[End.Y][End.X] = true;
 	for (int dir = 0; dir < 8; dir++) {
 		int nx = End.X + bombdx[dir];
 		int ny = End.Y + bombdy[dir];
@@ -183,8 +156,9 @@ void bombAttack(CD Start, CD End, int K) {
 		if (ny > N) ny = 1;
 		if (ny < 1) ny = N;
 		if (board[ny][nx] == 0) continue;
-		board[ny][nx] -= board[Start.Y][Start.X] + N + M;
-		if (board[End.Y][End.X] < 0) board[End.Y][End.X] = 0;
+		board[ny][nx] -= (board[Start.Y][Start.X] + N + M) / 2;
+		related[ny][nx] = true;
+		if (board[ny][nx] < 0) board[ny][nx] = 0;
 	}
 	attack[Start.Y][Start.X] = K;
 }
@@ -212,6 +186,7 @@ void prepare() {
 }
 
 void solve(int k) {
+	memset(related, false, sizeof(related));
 	CD Start = findMin();
 	CD End = findMax();
 	if (canLaser(Start, End)) {
@@ -239,6 +214,6 @@ int main() {
 	for (int k = 0; k < K; k++) {
 		solve(k);
 	}
-	cout << findAnswer() - 1;
+	cout << findAnswer();
 	return 0;
 }
